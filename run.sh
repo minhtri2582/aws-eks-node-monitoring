25bfd9c07ae3cea1a52e3772dd0fe827ae82024c
Tri Do <tri.do@cloudteam.vn>
2023-03-15 11:36:29 +0700

    k8s secret and deploy

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+# RUN docker
 docker run --env-file .env minhtri2582/aws-eks-node-monitoring:latest
+
+# Create k8s secret from .env
+ kubectl create secret generic check-node-status-secret --from-env-file=.env
